9663331bc7865d07c80ab13123327ec53d13331f
Volcan4436 <b3at3r02@gmail.com>
2025-02-24 16:16:26 +1100

    Added UnlockRecipes

--- src/main/java/adris/altoclef/altomenu/managers/ModuleManager.java
+++ src/main/java/adris/altoclef/altomenu/managers/ModuleManager.java
@@ -2,6 +2,7 @@ package adris.altoclef.altomenu.managers;
 
 import adris.altoclef.altomenu.Mod;
 import adris.altoclef.altomenu.modules.Baritone.ChatBot;
+import adris.altoclef.altomenu.modules.Exploit.UnlockRecipes;
 import adris.altoclef.altomenu.modules.Movement.*;
 import adris.altoclef.altomenu.modules.Render.Fullbright;
 import adris.altoclef.altomenu.modules.Render.RenderTags;
@@ -77,6 +78,10 @@ public class ModuleManager {
         modules.add(new AutoWalk());
         modules.add(new Flight());
         modules.add(new AdvancedFly());
+
+
+        //Exploit
+        modules.add(new UnlockRecipes());
 
         //Render
         modules.add(new Fullbright());
