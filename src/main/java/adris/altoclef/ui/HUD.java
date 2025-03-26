7e7a513501862f7bb23559388168262341d39c40
Volcan4436 <b3at3r02@gmail.com>
2025-03-26 13:03:18 +1100

    HUD :D

--- src/main/java/adris/altoclef/ui/HUD.java
+++ src/main/java/adris/altoclef/ui/HUD.java
@@ -2,6 +2,11 @@ package adris.altoclef.ui;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.realms.Ping;
+import net.minecraft.network.packet.s2c.query.PingResultS2CPacket;
+
+import java.awt.*;
+import java.util.Objects;
 
 public class HUD {
 
@@ -9,7 +14,24 @@ public class HUD {
 
     //WaterMark
     public static void render(DrawContext context, float tickDelta) {
-        context.drawText(mc.textRenderer, "AnarchyClef [BETA]", 3, 500, -1, false);
-    }
 
+
+        int screenWidth = mc.getWindow().getScaledWidth();
+        int screenHeight = mc.getWindow().getScaledHeight();
+
+
+        context.drawText(mc.textRenderer, "HP: " + mc.player.getHealth(), screenWidth - mc.textRenderer.getWidth("HP: " + mc.player.getHealth()) - 3, screenHeight - 10, Color.red.getRGB(), false);
+        String yawText = Math.round(-mc.player.getYaw() % 360) + " :Yaw";
+        String pitchText = Math.round(mc.player.getPitch()) + " :Pitch";
+
+        int yawX = screenWidth - 3 - mc.textRenderer.getWidth(yawText);
+        context.drawText(mc.textRenderer, yawText, yawX, screenHeight - 20, -1, false);
+
+        int pitchX = screenWidth - 3 - mc.textRenderer.getWidth(pitchText);
+        context.drawText(mc.textRenderer, pitchText, pitchX, screenHeight - 30, -1, false);
+        context.drawText(mc.textRenderer, "X: " + Math.round(mc.player.getX()) + " Y: " + Math.round(mc.player.getY()) + " Z: " + Math.round(mc.player.getZ()), screenWidth % 2 + 3, screenHeight - 40, -1, false);
+        context.drawText(mc.textRenderer, "UUID: " + mc.player.getUuid().toString(), screenWidth % 2 + 3, screenHeight - 30, -1, false);
+        context.drawText(mc.textRenderer, "FPS: " + mc.getCurrentFps(), screenWidth % 2 + 3, screenHeight - 20, Color.green.getRGB(), false);
+        context.drawText(mc.textRenderer, "AnarchyClef [BETA]", screenWidth % 2 + 3, screenHeight - 10, -1, false);
+    }
 }
