5b066acf4a2afebaf2e037708b5e0f549744ef20
Volcan4436 <b3at3r02@gmail.com>
2025-11-22 10:12:40 +1100

    b0.5.0

--- src/main/java/adris/altoclef/util/VersionUtil.java
+++ src/main/java/adris/altoclef/util/VersionUtil.java
@@ -4,8 +4,8 @@ public class VersionUtil {
 
     public static VersionUtil INSTANCE = new VersionUtil();
     public static String clientSoftwareVersion = "1.20.4";
-    public static String clientVersionRaw = "b0.3.0";
-    public static String clientVersion = "AnarchyClef - b0.4.3 Beta ";
-    public static String cvUpdateName = "Bind-Config Rewrite by ChiefWarcry!";
+    public static String clientVersionRaw = "b0.5.0";
+    public static String clientVersion = "AnarchyClef - b0.5 ";
+    public static String cvUpdateName = "< " + "I AM THE TERMINATOR 2.0 Update" + " >";
 
 }
