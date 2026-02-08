539bf581cef8d8739c00d3ad5b5b7f6195071ef0
Volcan4436 <b3at3r02@gmail.com>
2026-02-08 11:57:31 +1100

    Ready for Next Release

--- src/main/java/adris/altoclef/util/VersionUtil.java
+++ src/main/java/adris/altoclef/util/VersionUtil.java
@@ -5,7 +5,7 @@ public class VersionUtil {
     public static VersionUtil INSTANCE = new VersionUtil();
     public static String clientSoftwareVersion = "1.20.4";
     public static String clientVersionRaw = "b0.5.0";
-    public static String clientVersion = "AnarchyClef - b0.5 ";
+    public static String clientVersion = "AnarchyClef - b0.5.0";
     public static String cvUpdateName = "< " + "I AM THE TERMINATOR 2.0 Update" + " >";
 
 }
