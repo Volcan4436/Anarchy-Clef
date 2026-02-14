2c4a4c629f92ca7333297c5b692978ad6d92e26e
Volcan4436 <b3at3r02@gmail.com>
2026-02-14 14:07:16 +1100

    Added RotationUtil to simplify Rotation Math in the future

--- src/main/java/adris/altoclef/util/math/RotationUtil.java
+++ src/main/java/adris/altoclef/util/math/RotationUtil.java
@@ -4,13 +4,16 @@ import net.minecraft.util.math.MathHelper;
 
 import static adris.altoclef.altomenu.command.ChatUtils.mc;
 
+//todo: add a method for getting the Yaw and Pitch of any Entity(s) not just the player
 public class RotationUtil {
 
+    //Gets the true Pitch
     public static float getPitch() {
         assert mc.player != null;
         return MathHelper.clamp(mc.player.getPitch(), -90f, 90f);
     }
 
+    //Gets the true Yaw
     public static float getYaw() {
         assert mc.player != null;
         return ((mc.player.getYaw() + 180f) % 360f + 360f) % 360f - 180f;
